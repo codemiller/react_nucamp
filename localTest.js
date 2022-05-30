5eda11339f0af7ca86459de7a8b483caa73dc7a5
codemiller <ericzacharymiller@gmail.com>
2022-05-29 20:33:17 -0400

    destructuring

---
+++ localTest.js
@@ -0,0 +1,25 @@
+// function eric() {
+//     return {
+//         name: 'Eric',
+//         vertical: 80,
+//         wife: "Flexi"
+//     }
+// }
+
+// const { name } = eric();
+
+// console.log(name)
+
+
+function zach({height}) {
+    console.log(height)
+}
+
+zach({name: 'zach', height: 100})
+
+
+// hello = ['yes', 'no', 'maybe']
+
+// const [var1, var2, var3] = hello
+
+// console.log(var1)
